a1313a1720282a27a8eff0fa4b23c0ebe2ecec9f
hassen <hassenbj33@gmail.com>
2025-03-28 05:26:05 +0100

    Feat: Hero Section..

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Hero />
-      <FeaturedOptionsSection />
+      <Hero />
+      {/* <FeaturedOptionsSection />
       <MembersStoriesSection />
       <Footer /> */}
     </>
--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,10 +1,49 @@
 import styles from "./Hero.module.css";
 
+/**
+ * Hero Component
+ * ---------------
+ * - Displays a large heading, description, button, and image.
+ * - Uses flexbox for layout.
+ *
+ * @returns {JSX.Element} Hero section.
+ */
+
 const Hero = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.heroContainer}> Hero</div>
-    </div>
+    <section className={styles.container}>
+      <div className={styles.heroContainer}>
+        {/*
+        ============
+        Hero Content
+        ============
+        */}
+        <div className={styles.content}>
+          <h1>
+            Central Texas
+            <br />
+            Fly Fishing
+          </h1>
+          <p>
+            At lacus vitae nulla sagittis scelerisque nisl. Pellentesque duis
+            cursus vestibulum, facilisi ac, sed faucibus.
+          </p>
+          <button className={styles.button}>Get started</button>
+        </div>
+        {/*
+        ==========
+        Hero Image 
+        ==========
+        */}
+        <div className={styles.imageWrapper}>
+          <img
+            src="./heroSectionImage.png"
+            alt="Fishing in a lake"
+            className={styles.image}
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
